e70bf2eeb0a3f3a3694a45c4a95f445209d38b6b
Jan Edmark Arieta <edarieta09@gmail.com>
2024-02-26 00:30:29 +0000

    adds total cohort duration: dddd dd ls x q

--- 4_queries/average_assistance_req_wait_time.sql
+++ 4_queries/average_assistance_req_wait_time.sql
@@ -1,16 +1,2 @@
-Average Assistance Request Wait Time
-We need to know the average amount of time that students are waiting for an assistance request. This is the duration between an assistance request being created, and it being started.
-
-Instruction
-Calculate the average time it takes to start an assistance request.
-
-Return just a single column here.
-Expected Result:
-
- average_wait_time 
--------------------
- 00:08:46.258793
-(1 row)
-
 SELECT avg(assistance_requests.created_at - assistance_requests.started_at) as average_wait_time
 FROM assistance_requests;
---
+++ 4_queries/total_cohort_duration.sql
@@ -0,0 +1,6 @@
+ SELECT cohorts.name as cohort, sum(assistance_requests.completed_at - assistance_requests.started_at) as total_duration
+ FROM assistance_requests
+ JOIN students ON students.id = student_id
+ JOIN cohorts ON cohorts.id = cohort_id
+ GROUP BY cohorts.name
+ ORDER BY total_duration;
