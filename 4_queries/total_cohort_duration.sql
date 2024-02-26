d38c8a324d2238b6ffee55555f401f80ccdcbc8a
Jan Edmark Arieta <edarieta09@gmail.com>
2024-02-26 00:36:49 +0000

    adds total duration cohorts sql

--- 4_queries/total_cohort_duration.sql
+++ 4_queries/total_cohort_duration.sql
@@ -1,4 +1,4 @@
- SELECT cohorts.name as cohort, sum(assistance_requests.completed_at - assistance_requests.started_at) as total_duration
+SELECT cohorts.name as cohort, sum(assistance_requests.completed_at - assistance_requests.started_at) as total_duration
  FROM assistance_requests
  JOIN students ON students.id = student_id
  JOIN cohorts ON cohorts.id = cohort_id
